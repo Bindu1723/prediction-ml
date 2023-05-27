e1c4744337c19ab79dbc686fb1c46f46878e95a5
Tharun Vemula <tharunvind@gmail.com>
2023-05-27 12:07:21 +0530

    Improve Score

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import pickle
 from flask import Flask, render_template, request
 from scipy.special import inv_boxcox
 from os.path import join, dirname
+import numpy as np
 
-filename = join(dirname(__file__), "model", "final_model.pkl")
+filename = join(dirname(__file__), "model", "predictor.pkl")
+model = pickle.load(open(filename, "rb"))
+
+
 app = Flask(__name__)
-lambda_ = -0.34318951188220137
 
 
 @app.route("/")
@@ -15,44 +18,11 @@ def hello_word():
 
 @app.route("/submit", methods=["POST"])
 def find_price():
-    model = pickle.load(open(filename, "rb"))
-    bhk = int(request.form["bhk"])
-    size = int(request.form["size"])
-    area_type = int(request.form["areaType"])
-    city = int(request.form["city"])
-    furnishing_status = int(request.form["furnishingStatus"])
-    tenant_preferred = int(request.form["tenantsPreferred"])
-    bathroom = int(request.form["bathroom"])
-    point_of_contact = int(request.form["pointOfContact"])
-    floor_level = int(request.form["floorLevel"])
-    total_floors = int(request.form["totalFloors"])
-    month_posted = int(request.form["month"])
-    day_posted = int(request.form["day"])
-    day_of_the_week_posted = int(request.form["week"])
-    quarter_posted = int(request.form["quarter"])
+    raw_features = [int(x) for x in request.form.values()]
+    features = [np.array(raw_features)]
 
-    x = [
-        [
-            bhk,
-            size,
-            area_type,
-            city,
-            furnishing_status,
-            tenant_preferred,
-            bathroom,
-            point_of_contact,
-            floor_level,
-            total_floors,
-            month_posted,
-            day_posted,
-            day_of_the_week_posted,
-            quarter_posted,
-        ]
-    ]
-
-    prediction = model.predict(x)
-    rent = inv_boxcox(prediction, lambda_)[0]
-    rent = round(rent, -1)
+    prediction = model.predict(features)
+    rent = round(prediction[0])
     return render_template("result.html", rent=rent)
 
 
